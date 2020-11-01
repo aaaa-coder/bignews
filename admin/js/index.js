c817f5b404f6147b5b4d550ddc1b5a700c85421a
tangchaofu <1027402361@qq.com>
2020-11-01 20:49:21 +0800

    'user左边导航栏完成'

--- admin/js/index.js
+++ admin/js/index.js
@@ -22,8 +22,30 @@ function getUserInfo() {
 }
 
 getUserInfo();
-
+//用户退出
 function loginOut() {
     localStorage.removeItem('token');
     location.replace('./login.html');
 }
+//实现左边点击下拉的效果
+function article_slide() {
+    $('.menu .level02').slideToggle();
+    if ($('#level01_slide span').html().indexOf('文章管理') > -1) {
+        //循环添加类名，实现箭头转动
+        $('#level01_slide .icon-arrowdownl').toggleClass('rotate0');
+    }
+
+}
+
+$('.menu .level01').on('click', function () {
+    $(this).addClass('active').siblings().removeClass('active');
+})
+
+$('.menu .level02>li').on('click', function () {
+    // console.log($(this));
+    // console.log($(this).find('a'));
+    //因为active类名下才是a  所以需要给li加类名
+    $(this).addClass('active').siblings().removeClass('active')
+})
+
+
