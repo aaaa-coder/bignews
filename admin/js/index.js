fed8cbafffe30de666d593bf4e96425a07dddf51
tangchaofu <1027402361@qq.com>
2020-11-03 12:24:10 +0800

    '封装ajax请求网络层，并进行使用'

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,24 +1,15 @@
 //获取用户信息
 function getUserInfo() {
-    const token = localStorage.getItem('token');
-    $.ajax({
+    http.ajax({
         type: "get",
-        headers: { Authorization: token },
-        url: "http://localhost:8080/api/v1/admin/user/info",
+        url: '/admin/user/info',
         success: function (res) {
             const { nickname, userPic } = res.data;
             $('#user_info_icon').prop('src', userPic);
             $('#user_center_link_icon').prop('src', userPic);
             $('#user_info_name').html('欢迎&nbsp;&nbsp;' + nickname);
-        },
-        error: function (jqXHR, status) {
-            // console.log(jqXHR);
-            // console.log(status); error 
-            if (jqXHR.status === 403) {
-                location.replace('./login.html');
-            }
         }
-    });
+    })
 }
 
 getUserInfo();
--- admin/js/user.js
+++ admin/js/user.js
@@ -1,9 +1,7 @@
 function getUserInfo() {
-    const token = localStorage.getItem('token');
-    $.ajax({
+    http.ajax({
         type: "get",
-        headers: { Authorization: token },
-        url: "http://localhost:8080/api/v1/admin/user/detail",
+        url: '/admin/user/detail',
         success: function (res) {
             const { username, nickname, email, userPic, password } = res.data;
             if (res.code === 200) {
@@ -13,11 +11,7 @@ function getUserInfo() {
                 $('.user_pic').prop('src', userPic);
                 $('#password').val(password);
             }
-        }, error: function (jqXHR) {
-            if (jqXHR.status === 403) {
-                parent.window.location.replace('./login.html');
-            }
         }
-    });
+    })
 }
 getUserInfo()
