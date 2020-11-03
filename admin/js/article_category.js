91effb636ab337f024e30fd864a366b0bf3de5d3
tangchaofu <1027402361@qq.com>
2020-11-03 20:32:03 +0800

    '删除分类功能实现'

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -1,3 +1,4 @@
+//获取所有分类
 function getAll() {
     http.ajax({
         type: 'get',
@@ -6,6 +7,21 @@ function getAll() {
             // console.log(response);
             const renterResult = template('getAllTemplate', response);
             $('#tbody').html(renterResult);
+        }
+    })
+}
+//删除分类
+function delCategory(id) {
+    http.ajax({
+        type: 'post',
+        url: '/admin/category/delete',
+        data: { id },
+        success: function (response) {
+            if (response.code === 204) {
+                console.log(response);
+                alert(response.msg);
+                getAll();
+            }
         }
     })
 }
