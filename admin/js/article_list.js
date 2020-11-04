f9fd41b0e1bc5e8a2485c7b8358d3d59fc6b23dc
tangchaofu <1027402361@qq.com>
2020-11-04 16:36:01 +0800

    '实现分页显示数据'

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -9,18 +9,34 @@ function getCategoryList() {
         }
     })
 }
-function filter() {
+function filter(page) {
     const id = $('#selCategory').val();
     const status = $('#selStatus').val();
     http.ajax({
         type: 'get',
         url: '/admin/article/query',
-        data: { type: id, state: status },
+        data: { type: id, state: status, page },
         success: (res) => {
-            console.log(res);
+            // console.log(res);
             const renderResult = template('searchTemplate', res);
             $('tbody').html(renderResult);
+            initPagination(res.data.totalPage);
         }
     })
 }
+// 4、初始化分页组件
+function initPagination(total) {
+    $('#pagination').twbsPagination({
+        totalPages: total, // 总的页数
+        visiblePages: 5, // 显示多少页
+        first: '首页',
+        last: '末页',
+        prev: '上一页',
+        next: '下一页',
+        onPageClick: (event, page) => {
+            filter(page);
+        }
+    });
+}
 getCategoryList();
+
