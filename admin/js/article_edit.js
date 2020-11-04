447f539acceccc7af8a5a36b9d162ee9ae1e1c9e
tangchaofu <1027402361@qq.com>
2020-11-04 18:14:16 +0800

    '编辑文章中分类功能完成'

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -7,7 +7,7 @@ function getEditArticle() {
         url: '/admin/article/search',
         data: { id },
         success: (res) => {
-            console.log(res);
+            // console.log(res);
             const { title, cover, date, content } = res.data;
             $('#inputTitle').val(title);
             $('.article_cover').prop('src', cover);
@@ -16,4 +16,21 @@ function getEditArticle() {
         }
     })
 }
+
+//获取文章类别
+function getArticleCategory() {
+    http.ajax({
+        type: 'get',
+        url: '/admin/category/list',
+        success: (res) => {
+            // console.log(res);
+            const renderResult = template('categoryTemplate', res);
+            $('.form-control.category').html(renderResult);
+        }
+    })
+}
+
+//调用获取类别方法
+getArticleCategory()
+//调取获取编辑内容方法
 getEditArticle();
